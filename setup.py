253c8f736fabeeeaa3d002d48df3a877ac7f3ba2
Nicolas Crocfer <ncrocfer@gmail.com>
2021-01-13 23:19:06 +0100

    chore: add Python 3.8 in setup.py classifiers

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     entry_points={"console_scripts": ["opencve=opencve.cli:cli"]},
     python_requires=">=3.6",
