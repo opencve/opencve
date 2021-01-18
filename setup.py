783fb2f026cac6b4f8e3418d1aacd078c28cc8c6
Nicolas Crocfer <ncrocfer@gmail.com>
2021-01-18 18:31:39 +0100

    chore: set markdown as description content type

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
     author_email="ncrocfer@gmail.com",
     description="CVE Alerting Platform",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/opencve/opencve",
     packages=find_packages(),
     install_requires=requirements,
