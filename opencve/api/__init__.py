e21e422efe3caa636691a4933b6588a096a4f4fe
ChriZzn <25209291+ChriZzn@users.noreply.github.com>
2021-05-11 10:56:25 +0200

    Update __init__.py

--- opencve/api/__init__.py
+++ opencve/api/__init__.py
@@ -12,7 +12,10 @@ from opencve.api.products import (
 from opencve.api.reports import ReportListResource, ReportResource
 from opencve.api.vendors import VendorListResource, VendorResource, VendorCveResource
 
-from opencve.api.subscriptions import SubscriptionListRessourceVendor, SubscriptionListRessourceProduct
+from opencve.api.subscriptions import (
+    SubscriptionListRessourceVendor,
+    SubscriptionListRessourceProduct,
+)
 
 
 api_bp = Blueprint("api", __name__)
@@ -26,8 +29,8 @@ api.add_resource(CweCveResource, "/cwe/<string:id>/cve")
 api.add_resource(CveListResource, "/cve")
 api.add_resource(CveResource, "/cve/<string:id>")
 api.add_resource(ReportListResource, "/reports")
-api.add_resource(SubscriptionListRessourceVendor, "/subscriptions/vendor")
-api.add_resource(SubscriptionListRessourceProduct, "/subscriptions/product")
+api.add_resource(SubscriptionListRessourceVendor, "/account/subscriptions/vendors")
+api.add_resource(SubscriptionListRessourceProduct, "/account/subscriptions/products")
 api.add_resource(ReportResource, "/reports/<string:link>")
 api.add_resource(AlertListResource, "/reports/<string:link>/alerts")
 api.add_resource(AlertResource, "/reports/<string:link>/alerts/<string:id>")
