4ba8d060817969236f3a0efb5ad876479229e75b
ChriZzn <25209291+ChriZzn@users.noreply.github.com>
2021-05-15 09:56:32 +0200

    New API Endpoint (Subscriptions) (#99)

---
+++ opencve/api/subscriptions.py
@@ -0,0 +1,36 @@
+from flask import request
+from flask_restful import fields, marshal_with
+from flask_user import current_user, login_required
+from opencve.api.base import BaseResource
+from opencve.api.fields import HumanizedNameField
+from opencve.models.users import User
+import json
+
+vendor_list_fields = {
+    "name": fields.String(attribute="name"),
+    "human_name": HumanizedNameField(attribute="name"),
+}
+
+product_list_fields = {
+    "name": fields.String(attribute="name"),
+    "human_name": HumanizedNameField(attribute="name"),
+    "parent_vendor": fields.String(attribute="vendor.name"),
+}
+
+
+class SubscriptionListRessourceVendor(BaseResource):
+    @marshal_with(vendor_list_fields)
+    def get(self):
+        user = User.query.filter_by(
+            username=request.authorization.get("username")
+        ).first()
+        return user.vendors
+
+
+class SubscriptionListRessourceProduct(BaseResource):
+    @marshal_with(product_list_fields)
+    def get(self):
+        user = User.query.filter_by(
+            username=request.authorization.get("username")
+        ).first()
+        return user.products
