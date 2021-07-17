aa96e98fdf15f174339fd8506cc881f0873f7b74
Nicolas Crocfer <ncrocfer@gmail.com>
2021-07-17 10:51:04 +0200

    tests: add tests on last activities feature

--- tests/views/test_home.py
+++ tests/views/test_home.py
@@ -1,5 +1,9 @@
 from flask import request
 
+from opencve.extensions import db
+from opencve.models.users import User
+from opencve.models.vendors import Vendor
+
 
 def test_home_cve_redirect(client):
     response = client.get("/")
@@ -37,3 +41,97 @@ def test_home_terms(app, client):
     assert b"Terms of Service and Privacy Policy" in response.data
     response = client.get("/register")
     assert b"Terms of Service" in response.data
+
+
+def test_no_activity(client, login):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert (
+        b'You have to subscribe to <a href="/vendors">products and vendors</a> to see their last changes.'
+        in response.data
+    )
+
+
+def test_subscription_without_changes(client, login):
+    user = User.query.first()
+    vendor = Vendor(name="opencve")
+    db.session.add(vendor)
+    user.vendors.append(vendor)
+    db.session.commit()
+
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Your subscriptions haven't changed yet." in response.data
+
+
+def test_activity_new_cve(client, login, handle_events):
+    user = User.query.first()
+    handle_events("modified_cves/CVE-2018-18074.json")
+    user.vendors.append(Vendor.query.filter_by(name="canonical").first())
+    db.session.commit()
+
+    response = client.get("/")
+    assert response.status_code == 200
+    assert (
+        b'<a href="/cve/CVE-2018-18074">CVE-2018-18074</a> is a new CVE'
+        in response.data
+    )
+
+
+def test_activity_cve_changed(client, login, create_cve, handle_events):
+    create_cve("CVE-2018-18074")
+    user = User.query.first()
+    user.vendors.append(Vendor.query.filter_by(name="canonical").first())
+    db.session.commit()
+
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Your subscriptions haven't changed yet." in response.data
+
+    handle_events("modified_cves/CVE-2018-18074_references.json")
+    response = client.get("/")
+    assert response.status_code == 200
+    assert (
+        b'<a href="/cve/CVE-2018-18074">CVE-2018-18074</a> has changed' in response.data
+    )
+    assert (
+        b"<strong>1</strong> changed, <strong>1</strong> added, <strong>7</strong> removed"
+        in response.data
+    )
+
+
+def test_list_paginated(app, client, login, handle_events):
+    old = app.config["ACTIVITIES_PER_PAGE"]
+    app.config["ACTIVITIES_PER_PAGE"] = 2
+
+    user = User.query.first()
+    for cve in ["CVE-2018-18074", "CVE-2019-17052", "CVE-2020-26116"]:
+        handle_events(f"modified_cves/{cve}.json")
+    user.vendors.append(Vendor.query.filter_by(name="canonical").first())
+    user.vendors.append(Vendor.query.filter_by(name="linux").first())
+    user.vendors.append(Vendor.query.filter_by(name="python").first())
+    db.session.commit()
+
+    # Changes are ordered using the lastModifiedDate field of each CVE
+    response = client.get("/")
+    assert response.status_code == 200
+    assert (
+        b'<a href="/cve/CVE-2019-17052">CVE-2019-17052</a> is a new CVE'
+        in response.data
+    )
+    assert (
+        b'<a href="/cve/CVE-2020-26116">CVE-2020-26116</a> is a new CVE'
+        in response.data
+    )
+
+    response = client.get("/?page=2")
+    assert response.status_code == 200
+    assert (
+        b'<a href="/cve/CVE-2018-18074">CVE-2018-18074</a> is a new CVE'
+        in response.data
+    )
+
+    response = client.get("/?page=3")
+    assert response.status_code == 404
+
+    app.config["ACTIVITIES_PER_PAGE"] = old
