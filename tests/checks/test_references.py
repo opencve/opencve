f26531fbb45ee3c62206fcaedc76b5d2f6235c57
Nicolas Crocfer <nicolas.crocfer@ovhcloud.com>
2022-09-08 22:49:19 +0200

    tests: check modified CVE with quote in reference url

--- tests/checks/test_references.py
+++ tests/checks/test_references.py
@@ -84,3 +84,47 @@ def test_check_references(create_cve, handle_events, open_file):
             "http://lists.opensuse.org/opensuse-security-announce/2019-07/msg00024.html",
         ]
     )
+
+
+def test_check_references_with_quote(create_cve, handle_events, open_file):
+    cve = create_cve("CVE-2022-3122")
+    references = cve.json["cve"]["references"]["reference_data"]
+    assert len(references) == 2
+    assert [r["url"] for r in references] == [
+        "https://github.com/joinia/webray.com.cn/blob/main/Clinic's-Patient-Management-System/cpmssql.md",
+        "https://vuldb.com/?id.207854",
+    ]
+
+    # 1 modified reference
+    handle_events("modified_cves/CVE-2022-3122_references.json")
+    cve = Cve.query.filter_by(cve_id="CVE-2022-3122").first()
+
+    # Task has been created
+    tasks = Task.query.all()
+    assert len(tasks) == 1
+    task = tasks[0]
+
+    # Change has been created
+    changes = Change.query.all()
+    assert len(changes) == 1
+    change = changes[0]
+    assert change.task.id == task.id
+    references = change.json["cve"]["references"]["reference_data"]
+    assert len(references) == 2
+    assert sorted([r["url"] for r in references]) == sorted(
+        [
+            "https://github.com/joinia/webray.com.cn/blob/main/Clinic's-Patient-Management-System/cpmssql.md",
+            "https://vuldb.com/?id.207854",
+        ]
+    )
+
+    # Event has been created
+    events = Event.query.all()
+    assert len(events) == 1
+    event = events[0]
+    assert event.type == "references"
+
+    changed = event.details["changed"]
+    assert len(changed) == 1
+    assert changed[0]["old"]["tags"] == []
+    assert changed[0]["new"]["tags"] == ["Exploit", "Third Party Advisory"]
