0d6e18870e5cf7e0e19a1e0b64e47f9a3aab40d7
Nicolas Crocfer <ncrocfer@gmail.com>
2024-01-13 12:32:25 +0100

    wip: add importdb command

--- web/cves/management/commands/__init__.py
+++ web/cves/management/commands/__init__.py
@@ -1,18 +1,16 @@
-import pathlib
 import time
 from contextlib import contextmanager
 
 from django.conf import settings
 from django.core.management.base import BaseCommand as DjangoBaseCommand
-from django.db import connection
 
 
 class BaseCommand(DjangoBaseCommand):
-    mitre_path = settings.MITRE_REPO_PATH
-    nvd_path = settings.NVD_REPO_PATH
+    kb_path = settings.KB_REPO_PATH
+
     procedures = {
         "mitre": "CALL mitre_upsert(%(cve)s, %(created)s, %(updated)s, %(summary)s, %(path)s);",
-        "nvd": "CALL nvd_upsert(%(cve)s, %(created)s, %(updated)s, %(cvss)s, %(vendors)s, %(cwes)s, %(path)s);"
+        "nvd": "CALL nvd_upsert(%(cve)s, %(created)s, %(updated)s, %(cvss)s, %(vendors)s, %(cwes)s, %(path)s);",
     }
 
     def error(self, message, ending=None):
@@ -38,17 +36,3 @@ class BaseCommand(DjangoBaseCommand):
 
         elapsed_time = f"{round(time.time() - start, 3)}s"
         self.info(f"{end_msg} in {self.style.MIGRATE_LABEL(elapsed_time)}")
-
-    def repos_exist(self, paths):
-        if not all(
-            [
-                pathlib.Path.exists(pathlib.Path(p) / ".git")
-                for p in paths
-            ]
-        ):
-            return False
-        return True
-
-    def call_procedure(self, procedure, params):
-        with connection.cursor() as cursor:
-            cursor.execute(self.procedures.get(procedure), params)
--- web/cves/management/commands/importdb.py
+++ web/cves/management/commands/importdb.py
@@ -1,122 +1,51 @@
 import glob
 import json
 import pathlib
-import re
-import os
-from textwrap import dedent
 
-import arrow
-import git
+from django.db import connection
 from psycopg2.extras import Json
 
 from cves.management.commands import BaseCommand
-from cves.utils import vendors_conf_to_flat, weaknesses_to_flat
 
 
 class Command(BaseCommand):
-    def get_mitre_files(self):
-        return [
-            f
-            for f in glob.glob(self.mitre_path + "/**/*.json", recursive=True)
-            if re.search(r"CVE-\d{4}-\d{4,7}", f)
-        ]
+    CVE_UPSERT_PROCEDURE = """
+    CALL cve_upsert(
+        %(cve)s, %(created)s, %(updated)s, %(description)s, %(title)s, %(metrics)s, %(vendors)s, %(weaknesses)s
+    );
+    """
 
-    def get_nvd_cve_path(self, cve_id):
-        cve_year = cve_id.split("-")[1]
-        return pathlib.Path(self.nvd_path) / cve_year / f"{cve_id}.json"
+    def call_procedure(self, params):
+        with connection.cursor() as cursor:
+            cursor.execute(self.CVE_UPSERT_PROCEDURE, params)
 
-    @staticmethod
-    def get_nvd_data(cve_nvd_path):
-        with open(cve_nvd_path) as f:
-            cve_nvd_data = json.load(f)
+    def kb_repo_exist(self):
+        return pathlib.Path.exists(pathlib.Path(self.kb_path))
 
-        nvd_data = {
-            "vendors": vendors_conf_to_flat(cve_nvd_data.get("configurations")),
-            "weaknesses": weaknesses_to_flat(cve_nvd_data.get("weaknesses")),
-            "cvss": {"v20": None, "v30": None, "v31": None},
-        }
+    def insert_cve(self, path):
+        with open(path) as f:
+            cve = json.load(f)
 
-        # Parse the CVE scores
-        if "metrics" in cve_nvd_data:
-            cvss_keys = {
-                "v20": "cvssMetricV2",
-                "v30": "cvssMetricV30",
-                "v31": "cvssMetricV31",
-            }
-            for k, v in cvss_keys.items():
-                if v in cve_nvd_data["metrics"]:
-                    nvd_data["cvss"][k] = cve_nvd_data["metrics"][v][0]["cvssData"][
-                        "baseScore"
-                    ]
-
-        return nvd_data
-
-    def insert_cve(self, cve_file):
-        mitre_path = os.path.relpath(cve_file, self.mitre_path)
-
-        with open(cve_file) as f:
-            cve_data = json.load(f)
-
-        # All CVE have an associated ID
-        metadata = cve_data["cveMetadata"]
-        cve_id = metadata["cveId"]
-
-        # Rejected CVEs doesn't have published date
-        cve_created = metadata.get("datePublished") or metadata["dateReserved"]
-        cve_created_utc = arrow.get(cve_created).to("utc").datetime.isoformat()
-
-        # Recent CVEs doesn't have updated date
-        cve_updated = metadata.get("dateUpdated") or cve_created
-        cve_updated_utc = arrow.get(cve_updated).to("utc").datetime.isoformat()
-
-        # If no descriptions, take the rejected reasons
-        cna = cve_data["containers"]["cna"]
-        descriptions = cna.get("descriptions") or cna["rejectedReasons"]
-
-        # In case of multiple languages, keep the EN one
-        if len(descriptions) > 1:
-            descriptions = [d for d in descriptions if d["lang"] in ("en", "en-US")]
-        description = descriptions[0]["value"]
-
-        self.call_procedure(
-            procedure="mitre",
-            params={
-                "cve": cve_id,
-                "created": cve_created_utc,
-                "updated": cve_updated_utc,
-                "summary": description,
-                "path": Json({"mitre": mitre_path})
-            }
+        cve_data = cve.get("opencve")
+        params = dict(
+            cve_data,
+            **{
+                "metrics": Json(cve_data["metrics"]),
+                "vendors": Json(cve_data["vendors"]),
+                "weaknesses": Json(cve_data["weaknesses"]),
+            },
         )
-
-        # Handles the NVD data
-        cve_nvd_path = self.get_nvd_cve_path(cve_id)
-        if pathlib.Path.exists(cve_nvd_path):
-            nvd_data = self.get_nvd_data(cve_nvd_path)
-            nvd_path = os.path.relpath(str(cve_nvd_path), self.nvd_path)
-
-            self.call_procedure(
-                procedure="nvd",
-                params={
-                    "cve": cve_id,
-                    "created": cve_created_utc,
-                    "updated": cve_updated_utc,
-                    "cvss": Json(nvd_data["cvss"]),
-                    "vendors": Json(nvd_data["vendors"]),
-                    "weaknesses": Json(nvd_data["cweaknesses"]),
-                    "path": Json({"nvd": nvd_path}),
-                }
-            )
+        self.call_procedure(params)
 
     def handle(self, *args, **options):
-        if not self.repos_exist([self.mitre_path, self.nvd_path]):
-            self.error("The CVE repositories have to be cloned first")
+        if not self.kb_repo_exist():
+            self.error("The OpenCVE KB repository has to be cloned first")
             return
 
-        self.info(f"Parsing CVE repository ({self.blue(self.mitre_path)})")
-        cve_files = self.get_mitre_files()
+        self.info(f"Parsing the OpenCVE KB repository ({self.blue(self.kb_path)})")
+        files = glob.glob(self.kb_path + "/**/CVE*.json", recursive=True)
+        msg = f"Found {self.blue(len(files))} CVEs, adding them in database"
 
-        msg = f"Found {self.blue(len(cve_files))} CVEs, adding them in database"
         with self.timed_operation(msg):
-            for cve_file in sorted(cve_files):
-                self.insert_cve(cve_file)
+            for path in sorted(files):
+                self.insert_cve(path)
