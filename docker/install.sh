13633bc6c993b799f76808ae06ade36beb5c15be
Laurent Durnez <ldurnez@gmail.com>
2024-08-06 22:46:06 +0200

    fix: remove --depth 1 to keep the history

--- docker/install.sh
+++ docker/install.sh
@@ -55,11 +55,11 @@ start-docker-stack() {
 clone-repositories() {
 
     echo "--> Cloning OpenCVE needed repositories"
-    docker exec -it airflow-scheduler git clone --depth 1 --single-branch https://github.com/opencve/opencve-kb.git /home/airflow/repositories/opencve-kb
-    docker exec -it airflow-scheduler git clone --depth 1 --single-branch https://github.com/opencve/opencve-nvd.git /home/airflow/repositories/opencve-nvd
-    docker exec -it airflow-scheduler git clone --depth 1 --single-branch https://github.com/opencve/opencve-redhat.git /home/airflow/repositories/opencve-redhat
-    docker exec -it airflow-scheduler git clone --depth 1 --single-branch https://github.com/CVEProject/cvelistV5.git /home/airflow/repositories/cvelistV5
-    docker exec -it airflow-scheduler git clone --depth 1 --single-branch https://github.com/cisagov/vulnrichment.git /home/airflow/repositories/vulnrichment
+    docker exec -it airflow-scheduler git clone https://github.com/opencve/opencve-kb.git /home/airflow/repositories/opencve-kb
+    docker exec -it airflow-scheduler git clone https://github.com/opencve/opencve-nvd.git /home/airflow/repositories/opencve-nvd
+    docker exec -it airflow-scheduler git clone https://github.com/opencve/opencve-redhat.git /home/airflow/repositories/opencve-redhat
+    docker exec -it airflow-scheduler git clone https://github.com/CVEProject/cvelistV5.git /home/airflow/repositories/cvelistV5
+    docker exec -it airflow-scheduler git clone https://github.com/cisagov/vulnrichment.git /home/airflow/repositories/vulnrichment
 
 }
 
