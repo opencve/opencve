457fdac6994a169b25cac3097e0b88a150bb1a9c
Nicolas Crocfer <ncrocfer@gmail.com>
2024-09-07 23:20:05 +0200

    feat: use case insensitive search for vendors & products
    Signed-off-by: Nicolas Crocfer <ncrocfer@gmail.com>

--- web/tests/cves/test_views.py
+++ web/tests/cves/test_views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from bs4 import BeautifulSoup
 
 
 def test_list_cves_by_case_insensitive_vendors(db, create_cve, auth_client):
@@ -15,3 +16,21 @@ def test_list_cves_by_case_insensitive_vendors(db, create_cve, auth_client):
 
     response = client.get(f"{reverse('cves')}?vendor=Oracle&product=Commerce_Platform")
     assert response.status_code == 200
+
+
+def test_list_vendors_case_insensitive(db, create_cve, auth_client):
+    create_cve("CVE-2023-22490")
+    client = auth_client()
+
+    response = client.get(f"{reverse('vendors')}?search=Git-scm")
+    soup = BeautifulSoup(response.content, features="html.parser")
+    content = soup.find("table", {"id": "table-vendors"}).find_all("td")
+    assert content[0].text == "Git-scm"
+
+    response = client.get(f"{reverse('vendors')}?search=GIT")
+    soup = BeautifulSoup(response.content, features="html.parser")
+    content = soup.find("table", {"id": "table-vendors"}).find_all("td")
+    assert content[0].text == "Git-scm"
+    content = soup.find("table", {"id": "table-products"}).find_all("td")
+    assert content[0].text == "Git"
+    assert content[1].text == "Git-scm"
