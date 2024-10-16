1519720a5be2c49d4ac9519168045f01c26f7d69
Nicolas Crocfer <ncrocfer@gmail.com>
2024-10-16 23:24:25 +0200

    feat: add the matched subscriptions in notifications
    Signed-off-by: Nicolas Crocfer <ncrocfer@gmail.com>

--- scheduler/tests/test_notifiers.py
+++ scheduler/tests/test_notifiers.py
@@ -13,6 +13,7 @@ def test_prepare_payload(tests_path, tmp_path_factory):
         "organization_name": "orga1",
         "notification_name": "notification1",
         "notification_type": "webhook",
+        "project_subscriptions": ["foo", "foo$PRODUCT$bar"],
         "notification_conf": {
             "types": ["references"],
             "extras": {
@@ -83,6 +84,18 @@ def test_prepare_payload(tests_path, tmp_path_factory):
             "organization": "orga1",
             "project": "orga1-project1",
             "notification": "notification1",
+            "matched_subscriptions": {
+                "human": [
+                    "Bar",
+                    "Foo",
+                ],
+                "raw": ["foo", "foo$PRODUCT$bar"],
+            },
+            "subscriptions": {
+                "human": ["Bar", "Foo"],
+                "raw": ["foo", "foo$PRODUCT$bar"],
+            },
+            "title": "1 change on Bar, Foo",
             "period": {
                 "start": "2024-01-01T01:00:00+00:00",
                 "end": "2024-01-01T01:59:59+00:00",
@@ -93,6 +106,10 @@ def test_prepare_payload(tests_path, tmp_path_factory):
                         "cve_id": "CVE-2024-6962",
                         "description": "A vulnerability classified as critical was found in Tenda O3 1.0.0.10. This vulnerability affects the function formQosSet. The manipulation of the argument remark/ipRange/upSpeed/downSpeed/enable leads to stack-based buffer overflow. The attack can be initiated remotely. The exploit has been disclosed to the public and may be used. The identifier of this vulnerability is VDB-272116. NOTE: The vendor was contacted early about this disclosure but did not respond in any way.",
                         "cvss31": 8.8,
+                        "subscriptions": {
+                            "human": ["Bar", "Foo"],
+                            "raw": ["foo", "foo$PRODUCT$bar"],
+                        },
                     },
                     "events": [
                         {
