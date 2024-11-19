8352c45c9292e36d8bbec3203eea65781dc5f912
Nicolas Crocfer <ncrocfer@gmail.com>
2024-11-19 23:18:00 +0100

    refactor: remove duplicated code in organizations and onboarding middlewares
    Signed-off-by: Nicolas Crocfer <ncrocfer@gmail.com>

--- web/onboarding/middlewares.py
+++ web/onboarding/middlewares.py
@@ -2,8 +2,6 @@ from django.conf import settings
 from django.shortcuts import redirect
 from django.urls import resolve
 
-from organizations.models import Membership
-
 
 class OnboardingMiddleware:
     def __init__(self, get_response):
@@ -13,18 +11,7 @@ class OnboardingMiddleware:
         if not request.user.is_authenticated:
             return self.get_response(request)
 
-        # List all memberships associated to the user
-        memberships = (
-            Membership.objects.filter(
-                user=request.user,
-                role__in=[Membership.OWNER, Membership.MEMBER],
-                date_joined__isnull=False,
-            )
-            .order_by("organization__name")
-            .all()
-        )
-        organizations = [m.organization for m in memberships]
-
+        organizations = request.user.list_organizations()
         if not organizations and settings.ENABLE_ONBOARDING:
 
             current_view_name = resolve(request.path).view_name
