d8fb204cbb4c8b6319136552e795faeff9fdc0a8
Nicolas Crocfer <ncrocfer@gmail.com>
2024-12-01 20:06:31 +0100

    feat: add the hijack button in user admin view
    Signed-off-by: Nicolas Crocfer <ncrocfer@gmail.com>

--- web/users/admin.py
+++ web/users/admin.py
@@ -4,5 +4,10 @@ from django.contrib.auth.models import Group
 
 from users.models import User
 
-admin.site.register(User, UserAdmin)
+
 admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    change_form_template = "users/admin/change_form.html"
