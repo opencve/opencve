4f2601279896fd671c4303410be1254de6c2a477
Nicolas Crocfer <ncrocfer@gmail.com>
2026-03-05 18:31:50 +0100

    fix: remove microseconds from API datetime fields
    Signed-off-by: Nicolas Crocfer <ncrocfer@gmail.com>

--- web/cves/serializers.py
+++ web/cves/serializers.py
@@ -3,13 +3,28 @@ from rest_framework import serializers
 from cves.models import Cve, Product, Vendor, Weakness
 
 
+class DateTimeWithoutMicrosecondsField(serializers.DateTimeField):
+    """DateTime field that serializes without microseconds."""
+
+    def to_representation(self, value):
+        if value is None:
+            return None
+        return value.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
 class CveListSerializer(serializers.ModelSerializer):
+    created_at = DateTimeWithoutMicrosecondsField(read_only=True)
+    updated_at = DateTimeWithoutMicrosecondsField(read_only=True)
+
     class Meta:
         model = Cve
         fields = ["created_at", "updated_at", "cve_id", "description"]
 
 
 class CveDetailSerializer(serializers.ModelSerializer):
+    created_at = DateTimeWithoutMicrosecondsField(read_only=True)
+    updated_at = DateTimeWithoutMicrosecondsField(read_only=True)
+
     class Meta:
         model = Cve
         fields = [
