f3032c3fa190eaed2dfeea293d093897730c0a92
Nicolas Crocfer <ncrocfer@gmail.com>
2026-08-05 14:03:31 +0200

    refactor: remove unused OrganizationIsOwnerMixin
    Signed-off-by: Nicolas Crocfer <ncrocfer@gmail.com>

--- web/organizations/mixins.py
+++ web/organizations/mixins.py
@@ -4,7 +4,6 @@ from django.shortcuts import redirect
 from authorization.context import get_authorization_context
 from authorization.permissions import ORG_VIEW
 from authorization.policies import get_active_membership
-from authorization.roles import ORG_OWNER
 from authorization.mixins import RequiresOrgPermissionMixin
 
 
@@ -37,25 +36,8 @@ class OrganizationIsMemberMixin:
         return super().dispatch(request, *args, **kwargs)
 
 
-class OrganizationIsOwnerMixin:
-    """Owner-only organization role. Prefer RequiresOrgPermissionMixin for new code."""
-
-    def dispatch(self, request, *args, **kwargs):
-        if not request.current_organization:
-            messages.error(request, "The requested organization does not exist.")
-            return redirect("list_organizations")
-
-        membership = get_active_membership(request.user, request.current_organization)
-        if membership is None or membership.role != ORG_OWNER:
-            messages.error(request, "You are not an owner of the organization.")
-            return redirect("list_organizations")
-
-        return super().dispatch(request, *args, **kwargs)
-
-
 __all__ = [
     "OrganizationRequiredMixin",
     "OrganizationIsMemberMixin",
-    "OrganizationIsOwnerMixin",
     "RequiresOrgPermissionMixin",
 ]
--- web/tests/organizations/test_mixins.py
+++ web/tests/organizations/test_mixins.py
@@ -7,10 +7,8 @@ from django.views import View
 
 from organizations.mixins import (
     OrganizationIsMemberMixin,
-    OrganizationIsOwnerMixin,
     OrganizationRequiredMixin,
 )
-from organizations.models import Membership
 
 
 def _add_session_and_messages(request):
@@ -30,11 +28,6 @@ class _OrganizationRequiredView(OrganizationRequiredMixin, View):
 
 
 class _OrganizationIsMemberView(OrganizationIsMemberMixin, View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("ok")
-
-
-class _OrganizationIsOwnerView(OrganizationIsOwnerMixin, View):
     def get(self, request, *args, **kwargs):
         return HttpResponse("ok")
 
@@ -113,106 +106,3 @@ def test_organization_is_member_mixin_with_organization(
     assert response.content == b"ok"
     messages = list(request._messages)
     assert not messages
-
-
-@pytest.mark.django_db
-def test_organization_is_owner_mixin_without_organization(create_user, rf):
-    user = create_user()
-    request = rf.get("/")
-    request.user = user
-    request.current_organization = None
-    _add_session_and_messages(request)
-
-    view = _OrganizationIsOwnerView.as_view()
-    response = view(request)
-
-    assert response.status_code == 302
-    assert response.url == reverse("list_organizations")
-    messages = list(request._messages)
-    assert any(
-        message.message == "The requested organization does not exist."
-        for message in messages
-    )
-
-
-@pytest.mark.django_db
-def test_organization_is_owner_mixin_member_not_owner(
-    create_user, create_organization, rf
-):
-    owner = create_user(username="owner")
-    member = create_user(username="member")
-    organization = create_organization(name="orga1", user=owner, owner=True)
-
-    # Create a membership for the member with MEMBER role
-    Membership.objects.create(
-        user=member,
-        organization=organization,
-        role=Membership.MEMBER,
-    )
-
-    request = rf.get("/")
-    request.user = member
-    request.current_organization = organization
-    _add_session_and_messages(request)
-
-    view = _OrganizationIsOwnerView.as_view()
-    response = view(request)
-
-    assert response.status_code == 302
-    assert response.url == reverse("list_organizations")
-    messages = list(request._messages)
-    assert any(
-        message.message == "You are not an owner of the organization."
-        for message in messages
-    )
-
-
-@pytest.mark.django_db
-def test_organization_is_owner_mixin_invited_owner(
-    create_user, create_organization, rf
-):
-    owner = create_user(username="owner")
-    invited_owner = create_user(username="invited_owner")
-    organization = create_organization(name="orga1", user=owner, owner=True)
-
-    # Invited owner: date_joined is None so is_invited is True
-    Membership.objects.create(
-        user=invited_owner,
-        organization=organization,
-        role=Membership.OWNER,
-        date_joined=None,
-    )
-
-    request = rf.get("/")
-    request.user = invited_owner
-    request.current_organization = organization
-    _add_session_and_messages(request)
-
-    view = _OrganizationIsOwnerView.as_view()
-    response = view(request)
-
-    assert response.status_code == 302
-    assert response.url == reverse("list_organizations")
-    messages = list(request._messages)
-    assert any(
-        message.message == "You are not an owner of the organization."
-        for message in messages
-    )
-
-
-@pytest.mark.django_db
-def test_organization_is_owner_mixin_owner_ok(create_user, create_organization, rf):
-    user = create_user()
-    organization = create_organization(name="orga1", user=user, owner=True)
-
-    request = rf.get("/")
-    request.user = user
-    request.current_organization = organization
-    _add_session_and_messages(request)
-
-    view = _OrganizationIsOwnerView.as_view()
-    response = view(request)
-
-    # As an owner, the mixin should allow the request to proceed without redirect.
-    assert response.status_code == 200
-    assert response.content == b"ok"
